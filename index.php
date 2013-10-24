39198f1d5ac9a29593129b50e080c985c3647266
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2013-10-24 15:31:00 -0700

    revert error reporting because it wasn't working properly.

--- index.php
+++ index.php
@@ -54,8 +54,7 @@ if (defined('ENVIRONMENT'))
 
 		case 'testing':
 		case 'production':
-            error_reporting(E_ALL);
-            ini_set('display_errors',0);
+            error_reporting(0);
 		break;
 
 		default:
