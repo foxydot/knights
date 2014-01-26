dfc52ab3e80eaaea773782e504f75114957184ee
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2014-01-25 23:03:05 -0500

    hide search from non-logged in user

--- application/views/default/header.php
+++ application/views/default/header.php
@@ -2,10 +2,12 @@
 	<div class="container wrap logo-container">
 		<a href="/"><img id="logo" src="<?php print ADMIN_THEME_URL //TODO: Set up to use the logo in the admin panel. ?>/img/logo.png" alt="<?php print SITENAME; ?>" /></a>
 	</div>
+	<?php if($this->authenticate->check_auth()): ?>
 	<div class="search pull-right">
         <form action="/post/search" method="post">
             <input type="text" name="search_terms" id="search_terms" placeholder="Search" />
             <input type="submit" value="&#xf002;" />
         </form>
     </div> 
+    <?php endif; ?>
 </header>
