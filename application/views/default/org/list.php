508fbff437b7b80f411c9405b540ed7ca46c05aa
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2014-11-04 11:25:37 -0500

    add some cheater buttons, basic

--- application/views/default/org/list.php
+++ application/views/default/org/list.php
@@ -3,24 +3,36 @@
 		<div class="col-md-2">
 			<h4>Name</h4>
 		</div><!-- end col-md-3 -->
-		<div class="col-md-8">
+		<div class="col-md-6">
 			<h4>Description</h4>
 		</div><!-- end col-md-2 -->
-		<div class="col-md-2">
-			<h4>Date Added</h4>
-		</div><!-- end col-md-1 -->
+        <div class="col-md-2">
+            <h4>Date Added</h4>
+        </div><!-- end col-md-1 -->
+        <div class="col-md-1">
+            <h4>Visit</h4>
+        </div><!-- end col-md-1 -->
+        <div class="col-md-1">
+            <h4>Manage</h4>
+        </div><!-- end col-md-1 -->
 	</div><!-- end titleBar -->
 		<?php foreach($orgs AS $org){ ?>
 			<div class="stripe org clicky row" href="/org/edit/<?php print $org->ID; ?>">
 				<div class="col-md-2">
 					<h6 class="sub subheading"><?php print $org->name; ?></h6>
 				</div>
-				<div class="col-md-8">
+				<div class="col-md-6">
 					<h6 class="sub"><?php print $org->description; ?></h6>
 				</div>
-				<div class="col-md-2">
-					<h6 class="sub"><?php print date("F j, Y",$org->dateadded); ?></h6>
-				</div>				
+                <div class="col-md-2">
+                    <h6 class="sub"><?php print date("F j, Y",$org->dateadded); ?></h6>
+                </div>
+                <div class="col-md-1">
+                    <h6 class="sub"><a href="http://<?php print $org->description; ?>.msdlab3.com" class="pull-right btn btn-sm btn-default">Visit</a></h6>
+                </div>
+                <div class="col-md-1">
+                    <h6 class="sub"><a href="http://<?php print $org->description; ?>.msdlab3.com" class="pull-right btn btn-sm btn-default">Manage</a></h6>
+                </div>				
 			</div>
 			<?php } //end orgs ?>
 		<div id="footer" class="row">
